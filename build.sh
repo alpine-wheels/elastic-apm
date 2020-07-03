2215d19c73afccb582c9bb7f80c139e97eaf5188
William Jackson <william@subtlecoolness.com>
2020-07-03 11:07:53 -0500

    Simplify build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-rm -rf /workspace/elastic-apm
-/sbin/apk add --no-cache git
-ELASTIC_APM_VERSION="$(/usr/bin/awk -F '==' '/elastic-apm/ { print $2 }' requirements.txt)"
-/usr/bin/git config --global advice.detachedHead false
-/usr/bin/git clone --branch "v${ELASTIC_APM_VERSION}" --depth 1 https://github.com/elastic/apm-agent-python.git elastic-apm
-cd elastic-apm || exit
-/usr/local/bin/python setup.py bdist_wheel
+#!/bin/sh
+
+set -e
+
+# install os dependencies
+apk add --no-cache gcc
+
+# build
+pip wheel --no-deps --requirement requirements.txt
